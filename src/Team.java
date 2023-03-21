70b589ae7fc0ae83a8fb7ad4fa993d8eb68134e6
Sarah-Alsisi <sara_alsisi59@outlook.com>
2023-03-21 13:56:03 +0200

    update(): add team leader

--- src/Team.java
+++ src/Team.java
@@ -5,11 +5,13 @@ public class Team {
     private String name;
     private ArrayList<User> players;
     private int score;
-
+    // flag to track if the first player is the team leader
+    private boolean isLeader;
     public Team(String name) {
         this.name = name;
         this.players = new ArrayList<>();
         this.score = 0;
+        this.isLeader=true;
     }
     public void addPlayer(User player){
         players.add(player);
@@ -34,6 +36,12 @@ public class Team {
     }
     public void updateTeamScore(int score){
         this.score+=score;
+    }
+    public boolean isLeader() {
+        return isLeader;
+    }
+    public void setIsLeader(boolean isLeader) {
+        this.isLeader = isLeader;
     }
 
     public void updateScoreForAllPlayers() throws IOException {
